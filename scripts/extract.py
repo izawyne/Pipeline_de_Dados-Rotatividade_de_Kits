31370e5dd5ea4f639505bec83a6f7480ddd39341
Izabella Wyne <114692326+izawyne@users.noreply.github.com>
2026-04-28 11:21:09 -0300

    Update extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import os
 
+def extract():
+    print("Iniciando etapa EXTRACT...")
+
+    caminho = os.path.join("data_sample", "exemplo.csv")
+
+    df = pd.read_csv(caminho)
+
+    print("Dados extraídos com sucesso")
+    return df
+
+
+if __name__ == "__main__":
+    extract()
