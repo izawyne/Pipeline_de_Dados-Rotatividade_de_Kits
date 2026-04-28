49667a3a0e815102069ea76512ff3f8f430f80a0
Izabella Wyne <114692326+izawyne@users.noreply.github.com>
2026-04-28 11:21:51 -0300

    Update load.py

--- scripts/load.py
+++ scripts/load.py
@@ -1,1 +1,26 @@
+import os
 
+def load(df):
+    print("Iniciando etapa LOAD...")
+
+    # Regra de negócio (exemplo baseado no seu projeto)
+    df = df[df["quantidade"] > 5]
+
+    output_path = "output"
+    os.makedirs(output_path, exist_ok=True)
+
+    caminho_saida = os.path.join(output_path, "dados_tratados.csv")
+
+    df.to_csv(caminho_saida, index=False, sep=";")
+
+    print("Arquivo salvo com sucesso")
+    print(f"Caminho: {caminho_saida}")
+
+
+if __name__ == "__main__":
+    from transform import transform
+    from extract import extract
+
+    df = extract()
+    df = transform(df)
+    load(df)
